f2827c106438331134f48ec45865192bcbe825b8
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-05-31 23:58:29 +0530

    import  useState

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Sidebar.css'
 import Logo from '../../img/logo.png'
 import { SidebarData } from '../../Data/Data'
 import {UilSignOutAlt} from '@iconscout/react-unicons'
 
-
-
 export default function Sidebar() {
 
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState (0)
 
   return (
     <div className="Sidebar">
