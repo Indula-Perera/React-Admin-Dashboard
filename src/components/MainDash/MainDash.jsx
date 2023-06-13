ae1ba97e8ec2d6b4c7b4691e9b703e1c2a4aef1c
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-06-13 13:33:51 +0530

    Create Main Dash

---
+++ src/components/MainDash/MainDash.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './MainDash.css'
+
+export default function MainDash() {
+  return (
+    <div className='MainDash'>
+      <h1>Dashboard</h1>
+      
+    </div>
+  )
+}
