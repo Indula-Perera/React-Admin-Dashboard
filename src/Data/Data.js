e36b7aacea074c0754d5eaf634f983d1e2cdc126
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-07-12 14:29:03 +0530

    setup Progressbar

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -47,7 +47,7 @@ export const CardData =[
         color:{backGround:"linear-gradient(180deg,#bb67ff 0%, #c484f3 100%)",
         boxShadow: "0px 10px 20px 0px #e0c6f5",
     },
-    barValue:70,
+    barValue:70 ,
     value:"25,970.",
     png:UilUsdSquare,
     series:[
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import {useState} from 'react'
+import { AnimateSharedLayout } from 'framer-motion'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import './Card.css'
 
 export default function Card(props) {
+
+  const [expended, setExpanded] = useState(false)
+
   return (
-    <div className='Card'>
-      card
+   <AnimateSharedLayout>
+    {
+      expended?(
+          'Expanded'
+      ):
+      <CompactCard  param ={props} />
+    }
+   </AnimateSharedLayout>
+  )
+}
+
+// CompactCard
+
+function CompactCard ({param}){
+  const Png = param.png; 
+  return(
+    <div className="CompactCard"
+    style={{
+      background : param.color.backGround,
+      boxShadow: param.color.boxShadow
+    }}>
+      <div className="radialBar">
+        <CircularProgressbar 
+        value={param.barValue}
+        text={`${param.barValue}%`}
+        />
+      </div>
+      <div className="detail">
+        <Png/>
+        <span>${param.value}</span>
+        <span>Last 24 hours</span>
+      </div>
     </div>
   )
 }
