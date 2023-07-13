2c96b85cd69bd8113cdf8bf4c99d613b1eb4a85e
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-07-13 13:52:31 +0530

    recent orders

--- src/components/MainDash/MainDash.jsx
+++ src/components/MainDash/MainDash.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import './MainDash.css'
-import Cards from '../Cards/Cards'
-import Table from '../Table/Table'
+import React from "react";
+import "./MainDash.css";
+import Cards from "../Cards/Cards";
+import Table from "../Table/Table";
 
 export default function MainDash() {
   return (
-    <div className='MainDash'>
+    <div className="MainDash">
       <h1>Dashboard</h1>
-      <Cards/>
-      
-      <Table/>
-      
+      <Cards />
+
+      <Table />
     </div>
-  )
+  );
 }
